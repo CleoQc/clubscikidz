0578a87a1de98eef2908f60b7db170d54b0b1225
Nicole Parrot <cleoqc1124@gmail.com>
2020-07-28 16:32:41 -0400

    Reversing motors for ClubSciKids

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    gigglebot.turnMillisec(gigglebotWhichTurnDirection.Right, 1000)
+})
+input.onButtonPressed(Button.B, function () {
+    gigglebot.spinMillisec(gigglebotWhichTurnDirection.Right, 1000)
+})
+basic.forever(function () {
+	
+})
