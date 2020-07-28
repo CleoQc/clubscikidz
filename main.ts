2fdae68f89bd274f62015cfde66eced9479edcb4
Nicole Parrot <cleoqc1124@gmail.com>
2020-07-28 16:33:15 -0400

    Removing blocks from main.blocks

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    gigglebot.turnMillisec(gigglebotWhichTurnDirection.Right, 1000)
-})
-input.onButtonPressed(Button.B, function () {
-    gigglebot.spinMillisec(gigglebotWhichTurnDirection.Right, 1000)
-})
 basic.forever(function () {
 	
 })
